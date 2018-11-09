3adbf760c2cd7ec0c7f59b8bfca84c643dc5efc0
GM <myrgksshuri@gmail.com>
2018-11-09 17:10:16 +0900

    add: hello world command

---
+++ src/cli_main.py
@@ -0,0 +1,10 @@
+from world import world
+import hello
+
+
+def main():
+    print(hello.hello(), world.hello())
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/world/world.py
@@ -0,0 +1,6 @@
+def hello():
+    return 'World'
+
+
+if __name__ == '__main__':
+    print(hello())
